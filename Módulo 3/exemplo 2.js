5ebf079b18f0a3dfea0e50d9d85a1e7c0c5ada0d
WesleyRSC <wesley8rogerio@gmail.com>
2020-04-22 20:50:07 -0300

    exemplos JS

---
+++ Módulo 3/exemplo 2.js
@@ -0,0 +1,12 @@
+let name="React";
+let num=45;
+let isTrue=true;
+
+console.log(name);
+console.log(num);
+console.log(isTrue);
+
+const city = "Mauá - SP";
+const course = "Eng. da Computação";
+
+city="são paulo - Sp";
---
+++ Módulo 3/exemplo1.js
@@ -0,0 +1,21 @@
+console.log("Hello World");
+
+console.log(45);
+console.log(true);
+
+// comentariio de uma linha
+
+/* comentario de 
+ * multiplas linhas
+*/
+
+//Aritimetica
+console.log(`soma 5 + 2 = ${5+2}`);
+console.log(`Subtração 7 - 5 = ${7-5}`);
+console.log(`Multiplicação 8 * 4 = ${8*4}`);
+console.log(`Divisão 30 / 45 = ${30/45}`);
+console.log(`Potenciação 5 ** 2 = ${5**2}`);
+
+//Template String
+console.log("O valor é", 52-10);
+console.log(`O valor é ${52-10}`);
